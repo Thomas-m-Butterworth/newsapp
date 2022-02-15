3c8df7093609aa4b8a9d706891343a43421b8306
Thomas Butterworth <thomas.m.butterworth@gmail.com>
2022-02-15 11:53:20 +0000

    #3 console logs removed from debugging process

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,11 +1,7 @@
 const {selectTopics} = require("../models/topics.model");
 
-console.log(selectTopics)
-
 exports.getTopics = (req, res) => {
-    console.log('testing gettopics')
     selectTopics().then((topics) => {
-        console.log(topics)
     res.status(200).send({ topics });
   });
 };
--- models/topics.model.js
+++ models/topics.model.js
@@ -1,7 +1,6 @@
 const db = require("../db/connection");
 
 exports.selectTopics = () => {
-    console.log('test function')
   return db
     .query(`SELECT * from topics;`)
     .then((result) => result.rows)
