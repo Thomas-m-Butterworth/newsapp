79c47879b22121ce9e5c3881a20749981012f378
Thomas Butterworth <thomas.m.butterworth@gmail.com>
2022-02-16 18:01:06 +0000

    #21 GET Users enpoint built and tests. Error handling to be looked into in more depth when the PR with the error handlers is merged

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const {getTopics} = require("./controllers/topics.controller");
+const {getUsers} = require('./controllers/users.controllers')
 const app = express();
 
-
 app.get('/api/topics', getTopics);
-
+app.get('/api/users', getUsers)
 
 module.exports = app;
